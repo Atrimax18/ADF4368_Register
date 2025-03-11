374872ebd2a142d7079b480660b75ab2415e1648
Atrimax18 <alexey.tseirif@satixfy.com>
2025-03-11 13:14:03 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ADF4368_Register
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cmd_Exit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Cmd_Import = new System.Windows.Forms.Button();
+            this.Cmd_Write = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Cmd_Exit
+            // 
+            this.Cmd_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cmd_Exit.Location = new System.Drawing.Point(673, 543);
+            this.Cmd_Exit.Name = "Cmd_Exit";
+            this.Cmd_Exit.Size = new System.Drawing.Size(115, 68);
+            this.Cmd_Exit.TabIndex = 0;
+            this.Cmd_Exit.Text = "Exit";
+            this.Cmd_Exit.UseVisualStyleBackColor = true;
+            this.Cmd_Exit.Click += new System.EventHandler(this.Cmd_Exit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 138);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(548, 473);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ADF4368 REGISTER DATA:";
+            // 
+            // Cmd_Import
+            // 
+            this.Cmd_Import.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cmd_Import.Location = new System.Drawing.Point(673, 146);
+            this.Cmd_Import.Name = "Cmd_Import";
+            this.Cmd_Import.Size = new System.Drawing.Size(115, 51);
+            this.Cmd_Import.TabIndex = 2;
+            this.Cmd_Import.Text = "Import Files";
+            this.Cmd_Import.UseVisualStyleBackColor = true;
+            this.Cmd_Import.Click += new System.EventHandler(this.Cmd_Import_Click);
+            // 
+            // Cmd_Write
+            // 
+            this.Cmd_Write.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cmd_Write.Location = new System.Drawing.Point(673, 203);
+            this.Cmd_Write.Name = "Cmd_Write";
+            this.Cmd_Write.Size = new System.Drawing.Size(115, 56);
+            this.Cmd_Write.TabIndex = 3;
+            this.Cmd_Write.Text = "Write Register";
+            this.Cmd_Write.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "file path: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(536, 448);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 623);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Cmd_Write);
+            this.Controls.Add(this.Cmd_Import);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Cmd_Exit);
+            this.Name = "MainForm";
+            this.Text = "ADF4368 Register Control";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Cmd_Exit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Cmd_Import;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Cmd_Write;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ADF4368_Register
+{
+    public partial class MainForm: Form
+    {
+
+        string filepath = string.Empty;
+        Dictionary<string, string> regDB = new Dictionary<string, string>();
+
+        DataTable dt = new DataTable();
+        public MainForm()
+        {
+            InitializeComponent();
+
+            InitDataTable();
+        }
+
+        private void Cmd_Exit_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        public void InitDataTable()
+        {
+            dt.Columns.Add("Address", typeof(string));
+            dt.Columns.Add("Value", typeof(string));
+            dt.Columns.Add("Value byte", typeof(byte));
+            dataGridView1.DataSource = dt;
+            dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.ColumnHeader);
+            dataGridView1.AllowUserToAddRows = false;
+            
+        }
+
+        private void Cmd_Import_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ft = new OpenFileDialog())
+            {
+                try
+                {
+                    ft.InitialDirectory = Directory.GetCurrentDirectory();
+                    ft.Filter = "CSV Files (*.csv)|*.csv|All files (*.*)|*.*";
+                    ft.FilterIndex = 0;
+
+                    if (ft.ShowDialog() == DialogResult.OK)
+                    {
+                        filepath = ft.FileName;
+                        label2.Text = filepath;
+                        ParsingFile(filepath);
+
+                        foreach(var kvp in regDB)
+                        {
+                            dt.Rows.Add(kvp.Key, kvp.Value, Convert.ToByte(kvp.Value, 16));
+                        }
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Warning");
+                }
+            }
+
+        }
+
+        public void ParsingFile(string file)
+        {
+            if (!File.Exists(file))
+            {
+                MessageBox.Show("File not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                foreach (var line in File.ReadLines(file))
+                {
+                    var parts = line.Split(',');
+                    if (parts.Length >= 2)
+                    {
+                        string registerAddress = parts[0].Trim();
+                        string registerValue = parts[1].Trim();
+                        //byte registerValue = Convert.ToByte(parts[1], 16);//parts[1].Trim();
+                        //registers.Add((registerAddress, registerValue));
+                        regDB.Add(registerAddress, registerValue);
+                    }
+                }
+            }
+
+        }
+    }
+}
