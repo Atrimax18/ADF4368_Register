d95647fda53b299a9be57b1ac123abb9e1386538
Atrimax18 <alexey.tseirif@satixfy.com>
2025-03-26 09:56:53 +0200

    update gui

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -130,7 +130,7 @@
             // 
             // textValue
             // 
-            this.textValue.Location = new System.Drawing.Point(317, 53);
+            this.textValue.Location = new System.Drawing.Point(324, 53);
             this.textValue.Name = "textValue";
             this.textValue.Size = new System.Drawing.Size(115, 20);
             this.textValue.TabIndex = 6;
@@ -142,9 +142,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(15, 54);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(155, 16);
+            this.label3.Size = new System.Drawing.Size(165, 16);
             this.label3.TabIndex = 7;
-            this.label3.Text = "REGISTER ADDRES:";
+            this.label3.Text = "REGISTER ADDRESS:";
             // 
             // Cmd_ReadAll
             // 
@@ -173,7 +173,7 @@
             // 
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(176, 53);
+            this.comboBox1.Location = new System.Drawing.Point(186, 51);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 10;
